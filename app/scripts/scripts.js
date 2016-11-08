a96242f7af1bfd573e1c7bc10b99aa900821fdbd
CdrXander <xackhackman@gmail.com>
2016-11-08 16:43:25 -0700

    Main tasking done. Moving on to persistence of data

--- app/scripts/scripts.js
+++ app/scripts/scripts.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
-	$('#newTaskForm').hide();
-
+	//var and function declaration
 	var listo = [];
 	var Task = function(task) {
 		this.task = task;
@@ -19,8 +18,29 @@ $(document).ready(function() {
 		
 		}
 		$('#newTaskForm').slideToggle('fast','linear');
-	}
+	};
 
+	var advanceTask = function(task) {
+		var modified = task.innerText.trim();
+		for(var i = 0; i < listo.length; i++) {
+			if (listo[i].id === "new") {
+				listo[i].id = 'inProgress';
+			} else if (listo[i].id === "inProgress") {
+				listo[i].id = "archived";
+			} else {
+				listo.splice(i,1);
+			}
+			break;
+		}
+		task.remove();
+	};
+
+	//Execution
+	$('#newTaskForm').hide();
+
+
+
+	//Listeners
 	$('#saveNewItem').click(function(e) {
 		e.preventDefault();
 		var task = $('#newItemInput').val().trim();
@@ -31,11 +51,35 @@ $(document).ready(function() {
 	$('#add-todo').click(function() {
 		$('#newTaskForm').fadeToggle('fast','linear');
 	});
+
 	//Closes Form
 	$('#cancel').click(function() {
 		$('#newTaskForm').fadeToggle('fast','linear');
 	});
 	
+	//Move from "New" to "In Progress"
+	$(document).on('click', '#item', function(e) {
+		e.preventDefault();
+		var task = this;
+		advanceTask(task);
+		this.id = 'inProgress';
+		$('#currentList').append(this.outerHTML);
+	});
 
+	//Move from "In Progress" to "Complete"
+	$(document).on('click', '#inProgress', function(e) {
+		e.preventDefault();
+		var task = this;
+		task.id = "archived";
+		var changeIcon = task.outerHTML.replace('glyphicon-arrow-right', 'glyphicon-remove');
+		advanceTask(task);
+		$('#archivedList').append(changeIcon);
+	});
+
+	$(document).on('click', '#archived', function(e) {
+		e.preventDefault();
+		var task = this;
+		advanceTask(this);
+	});
 
 });
